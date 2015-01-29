7a4b5be5615edcbbdb1759bdfd372994075c1399
Tsutomu Ogasawara <tsutomu.ogasawara@mail.rakuten.com>
2015-01-29 14:23:59 +0900

    Initial Commit

---
+++ FrontendTaskSwitcher/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FrontendTaskSwitcher
+//
+//  Created by Ogasawara, Tsutomu | Oga | CWDD on 1/29/15.
+//  Copyright (c) 2015 Rakuten Front-end. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
