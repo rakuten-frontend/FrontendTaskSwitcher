48a1c854783f614d7ac0a63a27e70691b1b099f3
Tsutomu Ogasawara <tsutomu.ogasawara@mail.rakuten.com>
2015-01-29 15:45:55 +0900

    Divided menu controller from app delegate.

--- FrontendTaskSwitcher/AppDelegate.swift
+++ FrontendTaskSwitcher/AppDelegate.swift
@@ -13,25 +13,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-    @IBOutlet weak var taskMenu: NSMenu!
-    var statusItem : NSStatusItem!
-
-    private func setupMenu() {
-        let systemStatusBar = NSStatusBar.systemStatusBar()
-        //self.statusItem = systemStatusBar.statusItemWithLength(NSVariableStatusItemLength)
-        self.statusItem = systemStatusBar.statusItemWithLength(-1)
-        self.statusItem.highlightMode = true
-        self.statusItem.title = "Tasks"
-        self.statusItem.menu = self.taskMenu
-    }
-    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // setup menu
-        self.setupMenu()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
     }
-
 }
