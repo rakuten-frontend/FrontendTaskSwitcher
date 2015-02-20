219d21eb9329f3f2fc83a1b8dbe71668bca212cb
Tsutomu Ogasawara <tsutomu.ogasawara@mail.rakuten.com>
2015-02-20 14:02:21 +0900

    Installed pods

---
+++ FrontendTaskSwitcher/BridgeHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgeHeader.h
+//  FrontendTaskSwitcher
+//
+//  Created by Ogasawara, Tsutomu | Oga | CWDD on 2/19/15.
+//  Copyright (c) 2015 Rakuten Front-end. All rights reserved.
+//
+
+#ifndef FrontendTaskSwitcher_BridgeHeader_h
+#define FrontendTaskSwitcher_BridgeHeader_h
+
+
+#endif
