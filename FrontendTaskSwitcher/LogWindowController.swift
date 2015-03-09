54d8d66d18a8b22f7d058ae54dde1e74ee89394b
Tsutomu Ogasawara <tsutomu.ogasawara@mail.rakuten.com>
2015-03-09 22:18:33 +0900

    Added log window.

---
+++ FrontendTaskSwitcher/LogWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  LogWindowController.swift
+//  FrontendTaskSwitcher
+//
+//  Created by Ogasawara, Tsutomu | Oga | CWDD on 3/9/15.
+//  Copyright (c) 2015 Rakuten Front-end. All rights reserved.
+//
+
+import Cocoa
+
+class LogWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
